01f1ac1334a78bf24bd11b06f7a84198b90da9b4
Max VelDink <max.veldink@daveramsey.com>
2019-12-06 15:58:43 -0600

    Switch to while loop

--- day_two.rb
+++ day_two.rb
@@ -1,40 +1,48 @@
 
 
 def day_two(arr, target_number)
-  99.times do |one|
-    99.times do |two|
-      returned_array = day_two_part_one(arr, one, two)
-    end
-  end
-end
+  100.times do |noun|
+    100.times do |verb|
+      new_array = arr.clone
+      new_array[1] = noun
+      new_array[2] = verb
 
+      returned_array = day_two_part_one(new_array)
 
-def day_two_part_one(input_arr, switch_index_1, switch_index_2)
-  answer = input_arr
-  answer[1] = switch_index_1
-  answer[2] = switch_index_2
-  input_arr.each_with_index  do |element, index|
-    if (index % 4 == 0)
-      if (element == 99)
-        return answer
-      else
-        next_number = answer[index + 1]
-        second_number = answer[index + 2]
-        third_number = answer[index + 3]
-        if (element == 1)
-          answer[third_number] = answer[next_number] + answer[second_number]
-        else
-          answer[third_number] = answer[next_number] * answer[second_number]
-        end
+      if returned_array[0] == target_number
+        return noun * 100 + verb
       end
     end
   end
-  return answer
+
+  return "Could not find 19690720"
+end
+
+
+def day_two_part_one(input_arr)
+  currentIndex = 0
+  while (currentIndex < input_arr.count && input_arr[currentIndex] != 99)
+    num1 = input_arr[input_arr[currentIndex + 1]]
+    num2 = input_arr[input_arr[currentIndex + 2]]
+
+    input_arr[input_arr[currentIndex + 3]] = case input_arr[currentIndex]
+    when 1
+      num1 + num2
+    when 2
+      num1 * num2
+    else
+      0
+    end
+
+    currentIndex += 4
+  end
+
+  input_arr
 end
 
 
 
+p day_two_part_one([1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,5,19,23,1,6,23,27,1,27,10,31,1,31,5,35,2,10,35,39,1,9,39,43,1,43,5,47,1,47,6,51,2,51,6,55,1,13,55,59,2,6,59,63,1,63,5,67,2,10,67,71,1,9,71,75,1,75,13,79,1,10,79,83,2,83,13,87,1,87,6,91,1,5,91,95,2,95,9,99,1,5,99,103,1,103,6,107,2,107,13,111,1,111,10,115,2,10,115,119,1,9,119,123,1,123,9,127,1,13,127,131,2,10,131,135,1,135,5,139,1,2,139,143,1,143,5,0,99,2,0,14,0])[0]
 
-
-p day_two([1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,19,5,23,2,9,23,27,1,5,27,31,1,5,31,35,1,35,13,39,1,39,9,43,1,5,43,47,1,47,6,51,1,51,13,55,1,55,9,59,1,59,13,63,2,63,13,67,1,67,10,71,1,71,6,75,2,10,75,79,2,10,79,83,1,5,83,87,2,6,87,91,1,91,6,95,1,95,13,99,2,99,13,103,1,103,9,107,1,10,107,111,2,111,13,115,1,10,115,119,1,10,119,123,2,13,123,127,2,6,127,131,1,13,131,135,1,135,2,139,1,139,6,0,99,2,0,14,0],
+p day_two([1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,5,19,23,1,6,23,27,1,27,10,31,1,31,5,35,2,10,35,39,1,9,39,43,1,43,5,47,1,47,6,51,2,51,6,55,1,13,55,59,2,6,59,63,1,63,5,67,2,10,67,71,1,9,71,75,1,75,13,79,1,10,79,83,2,83,13,87,1,87,6,91,1,5,91,95,2,95,9,99,1,5,99,103,1,103,6,107,2,107,13,111,1,111,10,115,2,10,115,119,1,9,119,123,1,123,9,127,1,13,127,131,2,10,131,135,1,135,5,139,1,2,139,143,1,143,5,0,99,2,0,14,0],
 19690720)
